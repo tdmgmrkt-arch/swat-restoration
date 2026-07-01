d5997a07549d2f5ec4bdb13beb3c1d257cf3d006
kennethlerch <kenneth.lerch@gmail.com>
2026-07-01 11:17:12 -0700

    site launch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import { GoogleAnalytics } from "@next/third-parties/google"
 import "./globals.css"
 import { siteConfig } from "@/lib/site-config"
 import { canonicalUrl } from "@/lib/utils"
@@ -77,6 +78,9 @@ export default function RootLayout({
         {children}
         <AccessibilityWidget />
       </body>
+      {process.env.NEXT_PUBLIC_GA_ID && (
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+      )}
     </html>
   )
 }
